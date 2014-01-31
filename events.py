5b9728263da5ce4e4d69c8278bb3a088bfc040d5
arminakvn <armin.akhavan@gmail.com>
2014-01-31 11:41:12 -0500

    Create events.py

---
+++ events.py
@@ -0,0 +1,12 @@
+class event:
+    #class defining an address matching instance/event
+    info_muni             = [] # town in which the matching is taking place
+    info_street_address   = [] # info group street address string
+    info_house_number     = [] # info group house number
+    info_zip              = [] # info group info group zip code
+    info_id               = [] # info group id
+    parcel_muni           = [] # town in which the matching is taking place
+    parcel_street_address = [] # parcel street address string
+    parcel_house_number   = [] # parcel house number
+    parcel_zip            = [] # parcel info group zip code
+    parcel_mapcid         = [] # parcel's mapc_id   
