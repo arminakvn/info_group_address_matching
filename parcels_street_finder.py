c3c23294328b4c2b3e41fbab268f9180994e26f4
arminakvn <armin.akhavan@gmail.com>
2014-01-31 11:42:26 -0500

    Create parcels_street_finder.py

---
+++ parcels_street_finder.py
@@ -0,0 +1,23 @@
+def parcels_street_finder(info_street_address, muni_id):
+    from events import event
+    from Levenshtein import ratio as lratio
+    from parcels_in_muni_to_df import parcels_in_muni_to_df
+    parcel_sel_df_for_matching = parcels_in_muni_to_df(muni_id)
+    all_parcels_street = {}
+    for item in parcel_sel_df_for_matching.transpose():
+        e_parcel = event()
+        raw_address = item[1]
+        from all_address_parser import pars_street_name
+        e_parcel.parcel_street_address = pars_street_name(raw_address)
+        from all_address_parser import pars_house_number
+        e_parcel.parcel_house_number = pars_house_number(raw_address)
+        e_parcel.parcel_mapcid = item[0]
+        u_parcel_address = unicode(str(e_parcel.parcel_street_address).lower())
+        u_info_street_address = unicode(str(info_street_address).lower())
+        
+        matcher = lratio(u_parcel_address, u_info_street_address)
+         
+        if matcher > 0.8:
+            all_parcels_street.update({e_parcel.parcel_house_number: e_parcel.parcel_mapcid})
+            print "levenstein ratio in making parcel street for ", u_info_street_address, " and ", u_parcel_address, ": ", matcher
+    return all_parcels_street
