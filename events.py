bbdeb48f0d0aa81106049438b486dbcee1f28c17
arminakvn <armin.akhavan@gmail.com>
2014-02-04 13:07:29 -0500

    Update events.py

--- events.py
+++ events.py
@@ -12,3 +12,18 @@ class event:
     parcel_mapcid         = [] # parcel's mapc_id
     match_hn_lratio       = [] # levenshtein ration between the house numbers that mathched
     match_sn_lratio       = [] # levenshtein ration between the street names that matched
+    index_street_name     = [] # name of the street for which the index has made from the parcels
+    index_street_values   = [] # indexed values of the street which contains the address list created by the all_street_finder
+    index_look_up         = {} 
+    
+    def make_index_streets(self, street_name, muni_id):
+        from parcels_street_finder import parcels_street_finder
+        self.index_street_name = street_name
+        self.info_muni = muni_id
+        self.index_street_values = []
+        self.index_street_values.append(parcels_street_finder(self.index_street_name, self.info_muni))
+        self.index_look_up.update({self.index_street_name: self.index_street_values})
+
+        
+
+
